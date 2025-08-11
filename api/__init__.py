9c64ec1fb01f3184271e3fde20620530f4dad8d7
semantic-release <semantic-release>
2025-08-11 11:57:14 +0000

    release: 1.2.3
    Automatically generated by semantic-release

--- api/__init__.py
+++ api/__init__.py
@@ -12,4 +12,4 @@ Main components:
 - dependencies: Reusable dependencies for database and authentication
 """
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -7,6 +7,6 @@ Ordinaut system.
 
 from .tick import SchedulerService
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 __all__ = ['SchedulerService']
